9fa1fd733e3985ea5fb18d6249e6741186eb7980
tom <tnje2011@gmail.com>
2016-06-21 19:24:09 +0800

    create the first version

--- syndicate-web/src/main/java/com/syndicate/controller/admin/BlogController.java
+++ syndicate-web/src/main/java/com/syndicate/controller/admin/BlogController.java
@@ -63,7 +63,6 @@ public class BlogController {
         BlogEntity blog = blogRepository.findOne(id);
         modelMap.addAttribute("blog", blog);
         return "admin/blog/blogDetail";
-
     }
 
     // 修改博文内容，页面
